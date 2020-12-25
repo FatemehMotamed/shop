63aacf252bc43e27ff2851117859d1a2694134fa
mahankabir <mahan.kabir@gmail.com>
2020-12-25 10:50:31 +0330

    creat Mobile size 1-add Avatar 2-add Brands 3-add Footer 4-add Cards 5-add Tiles

---
+++ components/app/Mobile/Home/Brands.vue
@@ -0,0 +1,40 @@
+
+<template>
+
+<div>
+    <v-row class="justify-space-around text-center pt-10">
+        <div>
+            <v-img src="https://dkstatics-public.digikala.com/digikala-adservice-banners/1000022866.png?x-oss-process=image/quality,q_80"></v-img>
+        </div>
+        <div>
+            <v-img src="https://dkstatics-public.digikala.com/digikala-adservice-banners/1000022676.png?x-oss-process=image/quality,q_80"></v-img>
+        </div>
+        <div>
+            <v-img src="https://dkstatics-public.digikala.com/digikala-adservice-banners/1000010474.png?x-oss-process=image/quality,q_80"></v-img>
+        </div>
+    </v-row>
+    <v-row class="justify-space-around text-center pt-10">
+        <div>
+            <v-img src="https://dkstatics-public.digikala.com/digikala-adservice-banners/1000022678.png?x-oss-process=image/quality,q_80"></v-img>
+        </div>
+        <div>
+            <v-img src="https://dkstatics-public.digikala.com/digikala-adservice-banners/1000022682.png?x-oss-process=image/quality,q_80"></v-img>
+        </div>
+        <div>
+            <v-img src="https://dkstatics-public.digikala.com/digikala-adservice-banners/1000022680.png?x-oss-process=image/quality,q_80"></v-img>
+        </div>
+    </v-row>
+</div>
+    
+</template>
+
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
--- components/app/Mobile/Home/HomePageXs.vue
+++ components/app/Mobile/Home/HomePageXs.vue
@@ -1,4 +1,4 @@
-<template>
+<template >
      <v-app>
     <v-card
       class="mx-auto overflow-hidden"
@@ -20,8 +20,58 @@
         </v-list>
       </v-navigation-drawer>
 
+
+      <avatar class="pt-10"></avatar>
       <v-card-text>
-        The navigation drawer will appear from the bottom on smaller size screens.
+        <slide-group></slide-group>
+
+        <v-row class="justify-space-between text-center pt-10">
+            <div><nuxt-link to="#"></nuxt-link></div>
+            <div>محصولات پربازدید اخیر</div>
+        </v-row>
+        <group-card></group-card>
+
+        <guarantee></guarantee>
+
+        <v-row class="justify-space-between text-center pt-10">
+            <div><nuxt-link to="#">مشاهده همه</nuxt-link></div>
+            <div>از پرطرفدارترین برندها: باینت</div>
+        </v-row>
+        
+        <tile-group></tile-group>
+
+        <v-row class="justify-space-between text-center pt-10">
+            <div><nuxt-link to="#">مشاهده همه</nuxt-link></div>
+            <div>از پرطرفدارترین برندها: آر اِن اِس</div>
+        </v-row>
+        <tile-group></tile-group>
+
+        <v-row class="justify-space-between text-center pt-10">
+            <div><nuxt-link to="#">مشاهده همه</nuxt-link></div>
+            <div>از پرطرفدارترین برندها: زی</div>
+        </v-row>
+        <tile-group></tile-group>
+
+        <v-row class="justify-space-between text-center pt-10">
+            <div><nuxt-link to="#">مشاهده همه</nuxt-link></div>
+            <div>برندهای برتر</div>
+        </v-row>
+        <brands></brands>
+
+        <v-row class="justify-space-between text-center pt-10">
+          <div><nuxt-link to="#"></nuxt-link></div>
+          <div>منتخب جدیدترین محصولات</div>
+        </v-row>
+        <group-card></group-card>
+
+        <v-row class="justify-space-between text-center pt-10">
+            <div><nuxt-link to="#">مشاهده همه</nuxt-link></div>
+            <div>خانه طراحان ایرانی</div>
+        </v-row>
+        <brands></brands>
+
+        <footer-side></footer-side>
+
       </v-card-text>
     </v-card>
   </v-app>
@@ -30,16 +80,32 @@
 <script>
   import AppBarPage from "@/components/app/Mobile/Header/AppBarPage";
   import NavigationDrawerPage from "@/components/app/Mobile/Header/NavigationDrawerPage";
+  import GroupCard from "@/components/app/Mobile/Home/GroupCard.vue";
+  import SlideGroup from "@/components/core/SlideGroup.vue";
+  import Guarantee from "@/components/app/Mobile/Home/Guarantee.vue";
+  import TileGroup from "@/components/app/Mobile/Home/TileGroup.vue";
+  import Brands from '@/components/app/Mobile/Home/Brands.vue';
+  import FooterSide from '@/components/app/Mobile/Home/FooterSide.vue';
+  import Avatar from '@/components/app/Mobile/Home/Avatar.vue';
+
     export default {
         name: "HomePage",
         components:{
             AppBarPage:AppBarPage,
             NavigationDrawerPage:NavigationDrawerPage,
+            GroupCard: GroupCard,
+            SlideGroup,
+            Guarantee,
+            TileGroup,
+            Brands,
+            FooterSide,
+            Avatar,
         },
+          
         data(){
             return{
                 drawer: false,
-                menu:['man','woman','child'],
+                menu:['مردانه','woman','child'],
                 menu2:['tshirt','shoe','kif'],
                 menu3:['tshirt1','tshirt2','tshirt3'],
                 menu4:['shoe1','shoe2','shoe3'],
@@ -54,7 +120,6 @@
                 this.menu=[];
                 this.menu=data
             }
-
         },
     }
 </script>
