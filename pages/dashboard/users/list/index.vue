0f779b3f536935bd5fce64e9e952dddc16b6cca9
poulstar <info@poulstar.com>
2020-12-28 20:45:19 +0330

    add_table_user_list

--- pages/dashboard/users/list/index.vue
+++ pages/dashboard/users/list/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <custom-table :desserts="table_data" :headers="headers" ></custom-table>
+  <custom-table :list="table_data"></custom-table>
 </template>
 
 <script>
@@ -13,103 +13,26 @@ export default {
     CustomTable:CustomTable,
   },
 
-  data(){
-    return{
-      headers: [
-        {
-          text: 'نام',
-          align: 'start',
-          sortable: false,
-          value: 'fname',
-        },
-        { text: 'نام خانوادگی', value: 'lname' },
-        { text: 'کدملی', value: 'fat' },
-      ],
-      table_data: [
-        {
-          name: 'Frozen Yogurt',
-          calories: 200,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: '1%',
-        },
-        {
-          name: 'Ice cream sandwich',
-          calories: 200,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: '1%',
-        },
-        {
-          name: 'Eclair',
-          calories: 300,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: '7%',
-        },
-        {
-          name: 'Cupcake',
-          calories: 300,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: '8%',
-        },
-        {
-          name: 'Gingerbread',
-          calories: 400,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: '16%',
-        },
-        {
-          name: 'Jelly bean',
-          calories: 400,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: '0%',
-        },
-        {
-          name: 'Lollipop',
-          calories: 400,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: '2%',
-        },
-        {
-          name: 'Honeycomb',
-          calories: 400,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: '45%',
-        },
-        {
-          name: 'Donut',
-          calories: 500,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: '22%',
-        },
-        {
-          name: 'KitKat',
-          calories: 500,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: '6%',
-        },
-      ],
+    data(){
+        return{
+            table_data: []
+        }
+    },
 
-    }
-  }
+    created(){
+        this.fetchSomething();
+    },
+    methods: {
+      async fetchSomething() {
+          let self = this;
+
+          this.$axios.$post('/user/usersList').then(function(response) {
+              self.table_data = response.data;
+              // console.log(self.table_data);
+          });
+      }
+    },
+
 
 }
 </script>
