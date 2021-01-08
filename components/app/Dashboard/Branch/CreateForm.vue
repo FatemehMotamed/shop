fb957c9db4cf169dd2e01e6b4636f3d03d120ac0
poulstar <info@poulstar.com>
2021-01-09 01:58:47 +0330

    v1 validation branch

--- components/app/Dashboard/Branch/CreateForm.vue
+++ components/app/Dashboard/Branch/CreateForm.vue
@@ -1,35 +1,41 @@
 <template>
   <div class="main rounded-lg">
     <v-container >
-      <form @submit.prevent="registerBranch" v-model="valid" >
+      <form>
         <v-row>
           <v-col cols="12" md="6" lg="6" sm="6">
-            <v-text-field  dir="rtl"  label="" placeholder="نام شعبه" v-model="form_data.name" :rules="txtRules" required></v-text-field>
+            <v-text-field  dir="rtl"  label="" placeholder="نام شعبه" v-model="name"  :error-messages="nameErrors" @input="$v.name.$touch()" @blur="$v.name.$touch()"  required></v-text-field>
           </v-col>
           <v-col cols="12" md="6" lg="6" sm="6">
-            <v-text-field  dir="rtl"  label="" placeholder="شماره تلفن" v-model="form_data.phones"  :rules="numRules"  required></v-text-field>
+            <v-text-field  dir="rtl"  label="" placeholder="شماره تلفن" v-model="phones"  :error-messages="phonesErrors" @input="$v.phones.$touch()" @blur="$v.phones.$touch()"   required></v-text-field>
           </v-col>
         </v-row>
         <custom-select></custom-select>
         <v-row>
           <v-col cols="12" md="12" lg="12" sm="12">
-            <v-textarea rows="3" row-height="25" dir="rtl" label="" placeholder="آدرس" v-model="form_data.address"  :rules="txtRules"  required></v-textarea>
+            <v-textarea rows="3" row-height="25" dir="rtl" label="" placeholder="آدرس" v-model="address"    :error-messages="addressErrors" @input="$v.address.$touch()" @blur="$v.address.$touch()"   required></v-textarea>
           </v-col>
 
         </v-row>
         <v-row>
           <v-col cols="12" md="6" lg="6" sm="6">
-            <v-text-field  dir="rtl"  label="" placeholder="شماره فکس" v-model="form_data.fax"  :rules="numRules"  required></v-text-field>
+            <v-text-field  dir="rtl"  label="" placeholder="شماره فکس" v-model="fax"  :error-messages="faxErrors" @input="$v.fax.$touch()" @blur="$v.fax.$touch()"   required></v-text-field>
           </v-col>
           <v-col cols="12" md="6" lg="6" sm="6">
-            <v-text-field  dir="rtl"  label="" placeholder="کد پستی" v-model="form_data.postal_code"  :rules="postalRules"  required></v-text-field>
+            <v-text-field  dir="rtl"  label="" placeholder="کد پستی" v-model="postal_code"    :error-messages="postalErrors" @input="$v.postal_code.$touch()" @blur="$v.postal_code.$touch()"   required></v-text-field>
           </v-col>
         </v-row>
 
 
         <v-row>
           <v-col cols="12" md="2" lg="2" sm="2" offset-lg="5">
-            <custom-button txt="ثبت شعبه" bgcolor="#0ad3f7" fontcolor="black" fontsize="1.3" icon="mdi-source-fork" iconcolor="black" width="10vw" height="3vw"></custom-button>
+<!--            <custom-button txt="ثبت شعبه" bgcolor="#0ad3f7" fontcolor="black" fontsize="1.3" icon="mdi-source-fork" iconcolor="black" width="10vw" height="3vw"></custom-button>-->
+            <v-btn
+              class="mr-4"
+              @click="submit"
+            >
+              submit
+            </v-btn>
           </v-col>
         </v-row>
 
@@ -47,25 +53,42 @@
   import CustomTextbox from '@/components/core/dashboard/CustomTextbox'
   import CustomSelect from '@/components/core/dashboard/CustomSelect'
   import EventBuss from '@/assets/js/eventBus'
+  import { validationMixin } from 'vuelidate'
+  import { required, maxLength, email, minLength } from 'vuelidate/lib/validators'
   export default {
-  name: "CreateForm",
-  components:{
+    name: "CreateForm",
+    components:{
       CustomButton:CustomButton,
       CustomTextbox:CustomTextbox,
       CustomSelect:CustomSelect
     },
-    data(){
-      return{
-        valid:false,
-        form_data:{
-          name:'',
-          phones:'',
-          county:'',
-          city:'',
-          address:'',
-          postal_code:'',
-          fax:'',
-        },
+    mixins: [validationMixin],
+
+    validations: {
+      name: { required, maxLength: maxLength(10),minLength: minLength(6) },
+      phones: { required, maxLength: maxLength(11) },
+      address: { required, maxLength: maxLength(10) },
+      postal_code: { required, maxLength: maxLength(10) },
+      fax: { required, maxLength: maxLength(10) },
+
+      // email: { required, email },
+      // select: { required },
+      // checkbox: {
+      //   checked (val) {
+      //     return val
+      //   },
+      // },
+    },
+    data: () => ({
+        select: null,
+        name:'',
+        phones:'',
+        county:'',
+        city:'',
+        address:'',
+        postal_code:'',
+        fax:'',
+
         txtRules: [
           v => !!v || 'این فیلد الزامی است',
           v => v.length >=6 || 'لطفا بیشتر از 6 کاراکتر وارد کنید',
@@ -80,26 +103,104 @@
           v => v.length >=10 || 'لطفا 10 رقم وارد کنید',
           v => v.length <=10 || 'لطفا 10 رقم وارد کنید',
         ],
-      }
-    },
+
+  }),
 
     methods:{
       registerBranch() {
-        let self = this;
-        this.$axios.post('/branch', self.form_data).then((res)=>{
-          console.log(res);
-        })
+
       },
       set_state_city(item){
         // console.log('test',item)
-        this.form_data.county=item[0]
-        this.form_data.city=item[1]
-      }
+        this.county=item[0]
+        this.city=item[1]
+      },
+      submit () {
+        this.$v.$touch()
+        if (this.$v.$pendding || this.$v.$error) return;
+        alert('Data Submit');
+        // this.$v.$reset();
+        // this.resetData();
+        let form_data={
+          name:this.name,
+          phones:this.phones,
+          county:this.county,
+          city:this.city,
+          address:this.address,
+          postal_code:this.postal_code,
+          fax:this.fax,
+        }
+        let self = this;
+        this.$axios.post('/branch', form_data).then((res)=>{
+          console.log(res);
+        })
+      },
+
+    },
+    computed: {
+      checkboxErrors () {
+        const errors = []
+        if (!this.$v.checkbox.$dirty) return errors
+        !this.$v.checkbox.checked && errors.push('You must agree to continue!')
+        return errors
+      },
+      selectErrors () {
+        const errors = []
+        if (!this.$v.select.$dirty) return errors
+        !this.$v.select.required && errors.push('Item is required')
+        return errors
+      },
+      emailErrors () {
+        const errors = []
+        if (!this.$v.email.$dirty) return errors
+        !this.$v.email.email && errors.push('Must be valid e-mail')
+        !this.$v.email.required && errors.push('E-mail is required')
+        return errors
+      },
+      nameErrors () {
+        const errors = []
+        if (!this.$v.name.$dirty) return errors
+        !this.$v.name.maxLength && errors.push('Name must be at most 10 characters long')
+        !this.$v.name.minLength && errors.push('min 6')
+        !this.$v.name.required && errors.push('Name is required.')
+        return errors
+      },
+      phonesErrors () {
+        const errors = []
+        if (!this.$v.phones.$dirty) return errors
+        !this.$v.phones.maxLength && errors.push('Name must be at most 10 characters long')
+        !this.$v.phones.required && errors.push('Name is required.')
+        return errors
+      },
+      addressErrors () {
+        const errors = []
+        if (!this.$v.address.$dirty) return errors
+        !this.$v.address.maxLength && errors.push('Name must be at most 10 characters long')
+        !this.$v.address.required && errors.push('Name is required.')
+        return errors
+      },
+      postalErrors () {
+        const errors = []
+        if (!this.$v.postal_code.$dirty) return errors
+        !this.$v.postal_code.maxLength && errors.push('Name must be at most 10 characters long')
+        !this.$v.postal_code.required && errors.push('Name is required.')
+        return errors
+      },
+
+      faxErrors () {
+        const errors = []
+        if (!this.$v.fax.$dirty) return errors
+        !this.$v.fax.maxLength && errors.push('Name must be at most 10 characters long')
+        !this.$v.fax.required && errors.push('Name is required.')
+        return errors
+      },
 
     },
     mounted(item) {
       EventBuss.$on('set-city',item =>{this.set_state_city(item)})
-    }
+    },
+
+
   }
 </script>
 
