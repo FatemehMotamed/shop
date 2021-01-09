120f2703273bc77bd5c06bdf27e08db185063055
poulstar <info@poulstar.com>
2021-01-10 01:14:19 +0330

    numeric and alert

--- components/app/Dashboard/Branch/CreateForm.vue
+++ components/app/Dashboard/Branch/CreateForm.vue
@@ -1,47 +1,51 @@
 <template>
   <div class="main rounded-lg">
     <v-container >
+      <v-alert :value="alert_success" color="green" type="success" dark border="top" transition="scale-transition" dir="rtl" align="center">
+        شعبه با موفقیت ثبت شد
+      </v-alert>
+      <v-alert :value="alert_error" color="red" type="error" dark border="top" transition="scale-transition" dir="rtl" align="center">
+        لطفا فیلدها را به درستی پر کنید
+      </v-alert>
       <form>
         <v-row>
           <v-col cols="12" md="6" lg="6" sm="6">
             <v-text-field  dir="rtl"  label="" placeholder="نام شعبه" v-model="name"  :error-messages="nameErrors" @input="$v.name.$touch()" @blur="$v.name.$touch()"  required></v-text-field>
           </v-col>
           <v-col cols="12" md="6" lg="6" sm="6">
-            <v-text-field  dir="rtl"  label="" placeholder="شماره تلفن" v-model="phones"  :error-messages="phonesErrors" @input="$v.phones.$touch()" @blur="$v.phones.$touch()"   required></v-text-field>
+            <v-text-field  dir="rtl"  label="" placeholder="شماره تلفن" v-model="phones"  :error-messages="phonesErrors" @input="$v.phones.$touch()" @blur="$v.phones.$touch()" ></v-text-field>
           </v-col>
         </v-row>
         <custom-select></custom-select>
-        <div v-bind="city_validation">{{city_validation}}</div>
+        <div class="validation_message" :value="city_validation">{{city_validation}}</div>
         <v-row>
           <v-col cols="12" md="12" lg="12" sm="12">
-            <v-textarea rows="3" row-height="25" dir="rtl" label="" placeholder="آدرس" v-model="address"    :error-messages="addressErrors" @input="$v.address.$touch()" @blur="$v.address.$touch()"   required></v-textarea>
+            <v-textarea rows="3" row-height="25" dir="rtl" label="" placeholder="آدرس" v-model="address"    :error-messages="addressErrors" @input="$v.address.$touch()" @blur="$v.address.$touch()"  ></v-textarea>
           </v-col>
 
         </v-row>
         <v-row>
           <v-col cols="12" md="6" lg="6" sm="6">
-            <v-text-field  dir="rtl"  label="" placeholder="شماره فکس" v-model="fax"  :error-messages="faxErrors" @input="$v.fax.$touch()" @blur="$v.fax.$touch()"   required></v-text-field>
+            <v-text-field  dir="rtl"  label="" placeholder="شماره فکس" v-model="fax"  :error-messages="faxErrors" @input="$v.fax.$touch()" @blur="$v.fax.$touch()"></v-text-field>
           </v-col>
           <v-col cols="12" md="6" lg="6" sm="6">
-            <v-text-field  dir="rtl"  label="" placeholder="کد پستی" v-model="postal_code"    :error-messages="postalErrors" @input="$v.postal_code.$touch()" @blur="$v.postal_code.$touch()"   required></v-text-field>
+            <v-text-field  dir="rtl"  label="" placeholder="کد پستی" v-model="postal_code"    :error-messages="postalErrors" @input="$v.postal_code.$touch()" @blur="$v.postal_code.$touch()"></v-text-field>
           </v-col>
         </v-row>
 
 
         <v-row>
-          <v-col cols="12" md="2" lg="2" sm="2" offset-lg="5">
+          <v-col cols="12" md="4" lg="4" sm="4" xs="4"></v-col>
+          <v-col cols="12" md="6" lg="6" sm="6" xs="6">
 <!--            <custom-button txt="ثبت شعبه" bgcolor="#0ad3f7" fontcolor="black" fontsize="1.3" icon="mdi-source-fork" iconcolor="black" width="10vw" height="3vw"></custom-button>-->
-            <v-btn
-              class="mr-4"
-              @click="submit"
-            >
-              submit
-            </v-btn>
+            <v-btn class="ml-6" @click="submit" color="primary" dark> submit</v-btn>
+            <v-btn class="ml-2" @click="submit" color="primary" dark> submit</v-btn>
           </v-col>
         </v-row>
 
 
       </form>
+
     </v-container>
 
   </div>
@@ -55,7 +59,7 @@
   import CustomSelect from '@/components/core/dashboard/CustomSelect'
   import EventBuss from '@/assets/js/eventBus'
   import { validationMixin } from 'vuelidate'
-  import { required, maxLength, email, minLength } from 'vuelidate/lib/validators'
+  import { required, maxLength, email, minLength, numeric } from 'vuelidate/lib/validators'
   export default {
     name: "CreateForm",
     components:{
@@ -67,13 +71,15 @@
 
     validations: {
       name: { required, minLength: minLength(6) },
-      phones: { required, minLength: minLength(11) },
-      address: { required, minLength: minLength(6) },
-      postal_code: { required, minLength: minLength(10) },
-      fax: { required, minLength: minLength(11) },
-      // select: { required },
+      phones: { minLength: minLength(11), maxLength: maxLength(11),numeric },
+      address: { minLength: minLength(6) },
+      postal_code: { minLength: minLength(10), maxLength: maxLength(10),numeric },
+      fax: { minLength: minLength(11), maxLength: maxLength(11),numeric },
+
     },
     data: () => ({
+        alert_success:false,
+        alert_error:false,
         city_validation:'',
         name:'',
         phones:'',
@@ -97,10 +103,13 @@
           this.city_validation="لطفا شهر و استان را انتخاب کنید"
         }
 
-        if (this.$v.$pendding || this.$v.$error || this.county=='' || this.city=='') return;
-        alert('Data Submit');
-        // this.$v.$reset();
-        // this.resetData();
+        if (this.$v.$pendding || this.$v.$error || this.county=='' || this.city==''){
+          this.alert_error=!this.alert_error;
+          return
+        }
+        this.alert_error=!this.alert_error
+        this.alert_success=!this.alert_success
+        this.$v.$reset();
         let form_data={
           name:this.name,
           phones:this.phones,
@@ -118,47 +127,42 @@
 
     },
     computed: {
-
-      selectErrors () {
-        const errors = []
-        if (!this.$v.select.$dirty) return errors
-        !this.$v.select.required && errors.push('Item is required')
-        return errors
-      },
       nameErrors () {
         const errors = []
         if (!this.$v.name.$dirty) return errors
-        !this.$v.name.minLength && errors.push('min 6')
-        !this.$v.name.required && errors.push('Name is required.')
+        !this.$v.name.minLength && errors.push('نام شعبه باید حداقل 6 کاراکتر باشد')
+        !this.$v.name.required && errors.push('این فیلد الزامی است')
         return errors
       },
       phonesErrors () {
         const errors = []
         if (!this.$v.phones.$dirty) return errors
-        !this.$v.phones.minLength && errors.push('Name must be at most 10 characters long')
-        !this.$v.phones.required && errors.push('Name is required.')
+        !this.$v.phones.minLength && errors.push('لطفا 11 رقم وارد کنید')
+        !this.$v.phones.maxLength && errors.push('لطفا 11 رقم وارد کنید')
+        !this.$v.phones.numeric && errors.push('لطفا فقط عدد وارد کنید')
         return errors
       },
       addressErrors () {
         const errors = []
         if (!this.$v.address.$dirty) return errors
-        !this.$v.address.minLength && errors.push('Name must be at most 10 characters long')
-        !this.$v.address.required && errors.push('Name is required.')
+        !this.$v.address.minLength && errors.push('آدرس باید حداقل 6 کاراکتر باشد')
         return errors
       },
       postalErrors () {
         const errors = []
         if (!this.$v.postal_code.$dirty) return errors
-        !this.$v.postal_code.minLength && errors.push('Name must be at most 10 characters long')
-        !this.$v.postal_code.required && errors.push('Name is required.')
+        !this.$v.postal_code.minLength && errors.push('لطفا 10 رقم وارد کنید')
+        !this.$v.postal_code.maxLength && errors.push('لطفا 10 رقم وارد کنید')
+        !this.$v.postal_code.numeric && errors.push('لطفا فقط عدد وارد کنید')
         return errors
       },
 
       faxErrors () {
         const errors = []
         if (!this.$v.fax.$dirty) return errors
-        !this.$v.fax.minLength && errors.push('Name must be at most 10 characters long')
-        !this.$v.fax.required && errors.push('Name is required.')
+        !this.$v.fax.minLength && errors.push('لطفا 11 رقم وارد کنید')
+        !this.$v.fax.maxLength && errors.push('لطفا 11 رقم وارد کنید')
+        !this.$v.fax.numeric && errors.push('لطفا فقط عدد وارد کنید')
         return errors
       },
 
@@ -191,5 +195,9 @@
   border-color: #0ad3f7 !important;
   color: black !important;
 }
+  .validation_message{
+    color:#ff5252 !important;
+    font-size: 12px;
+  }
 
 </style>
