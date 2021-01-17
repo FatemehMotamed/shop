91436381d759d4f9955bbdcedae580284bb953a8
mahankabir <mahan.kabir@gmail.com>
2021-01-17 19:47:06 +0330

    modified main mobile size 02

---
+++ components/app/Mobile/Home/ProductTile.vue
@@ -0,0 +1,36 @@
+<template>
+
+    <div class="gray">
+        <div class="red darken-3" style="width:9vw; height:4vw; float:right">
+            <span><strong class="red--text text--lighten-5">{{ product.off }}</strong></span>
+        </div>
+        <v-img
+        lazy-src="https://picsum.photos/id/11/10/6"
+        max-height="150"
+        max-width="125"
+        :src="product.image"
+        >
+        </v-img>
+        <v-row class="justify-space-between text-center">
+            <div class="pa-4"><strong><span>{{ product.price }}</span></strong></div>
+            <div class="pa-4"><strong>{{ product.brand }}</strong></div>
+        </v-row>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    props:{
+        product:{
+            type: Object,
+            require: true,
+        },
+    },
+    
+}
+</script>
+
+<style scoped>
+
+</style>
